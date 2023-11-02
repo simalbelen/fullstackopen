7f0c563c360ab4af01337c02097aeaed7b1bab7e
Belen Simal <simalbelen@gmail.com>
2023-11-02 14:28:30 +0100

    updated baseUrl

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "http://localhost:3001/api/persons"; // /api/persons for production deploy
 
 const getAll = () => {
     return axios.get(baseUrl);
