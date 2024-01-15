878a8b6e9a5a41fab64d1f26bac9b38d3b15071c
Belen Simal <simalbelen@gmail.com>
2024-01-15 20:43:57 +0100

    4.18 done

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,7 @@ const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -27,6 +28,7 @@ app.use(middleware.requestLogger)
 // Controllers
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
